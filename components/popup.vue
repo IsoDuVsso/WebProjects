285482d1ced307b930f20216191612abbab63a3b
Isaac COFFI <zakzakdu92@hotmail.fr>
2022-03-25 14:32:19 +0100

    Add files via upload

---
+++ components/popup.vue
@@ -0,0 +1,23 @@
+    <script>
+        export default {
+            name: 'Popup'
+        };
+    </script>
+    
+    <template>
+        <div class="Popup">
+            <div class="container">
+                <p>Bienvenue sur mon application test pour Bonita</p>
+                
+                <button class="mt-3 border-b border-teal font-semibold">Close</button>
+            </div>
+        </div>
+    </template>
+    
+    
+    <style lang="css" scoped>
+        .Popup {
+          background-color: rgba(0, 0, 0, 0.7);
+        }
+
+    </style>
